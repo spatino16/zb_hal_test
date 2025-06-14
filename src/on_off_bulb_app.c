eb95b4e1a7a704b4849341324d19d74b310b2ee9
spatino <spatino@tridentiot.com>
2025-06-13 19:27:18 -0500

    RTC init and handlder

--- src/on_off_bulb_app.c
+++ src/on_off_bulb_app.c
@@ -11,6 +11,8 @@
 #include "tr_af.h"
 #include "tr_hal_config.h"
 #include "project_app_tokens.h"
+#include "tr_hal_rtc.h"
+
 
 typedef struct
 {
@@ -29,6 +31,16 @@ static void led_blink(tr_hal_gpio_pin_t led_pin,
                       zb_uint16_t       off_ms,
                       zb_uint8_t        blink_count);
 static void led_blink_handler(zb_uint8_t param);
+
+static void rtc_event_handler(uint32_t events, tr_hal_rtc_date_time now)
+{
+    tr_app_printf("RTC EVENT: 0x%08lx at %02d:%02d:%02d\n",
+                  events,
+                  now.time.hours,
+                  now.time.minutes,
+                  now.time.seconds);
+}
+
 
 void tr_hal_button_interrupt_cb(tr_hal_gpio_pin_t   pin,
                                 tr_hal_gpio_event_t event)
@@ -59,7 +71,17 @@ void tr_app_init_cb(void)
     {
         led_blink(GPIO_LED_GREEN, 2000, 1, 1);
     }
+
+    // RTC setup
+    tr_hal_rtc_settings_t rtc_cfg = DEFAULT_RTC_CONFIG;
+
+    rtc_cfg.event_handler_fx = rtc_event_handler;
+    rtc_cfg.seconds_event_trigger = TR_HAL_EVENT_TRIGGER_ON_UNIT_CHANGE;
+
+    tr_hal_status_t rtc_status = tr_hal_rtc_init(&rtc_cfg);
+    tr_app_printf("RTC init: %s\n", rtc_status == TR_HAL_SUCCESS ? "OK" : "FAIL");
 }
+
 
 void tr_connection_state_cb(tr_conn_state_e conn_state)
 {
